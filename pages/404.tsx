5d2e79050a2e7be8e23ba972e08d12d4bdd6f2b4
Даня <45875910+danyatochkaru@users.noreply.github.com>
2022-11-28 15:44:40 +0300

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<main>
+			<Head>
+				<title>Piybeep</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<h1>404 | Страница не найдена</h1>
+		</main>
+	);
+}
