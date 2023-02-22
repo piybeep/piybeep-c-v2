39c5f921e0cc14c0e864b8856dd39affa35ee79b
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-02-22 09:20:29 +0300

    fix nopener

--- src/components/Privacy/Privacy.tsx
+++ src/components/Privacy/Privacy.tsx
@@ -9,7 +9,7 @@ export function Privacy({ title, subtitle }: PrivacyProps) {
         <div className={s.privacy}>
             <input className={s.privacy__checkbox} type="checkbox" id="checkbox" />
             <span className={s.privacy__agree}></span>
-            <label className={s.privacy__label} htmlFor="checkbox">{title} <a className={s.privacy__link} href="https://piybeep.com" target='_blank' rel="noreferrer">{subtitle}</a></label>
+            <label className={s.privacy__label} htmlFor="checkbox">{title} <a className={s.privacy__link} href="https://piybeep.com" target='_blank' rel="noopener">{subtitle}</a></label>
         </div>
     );
 };
