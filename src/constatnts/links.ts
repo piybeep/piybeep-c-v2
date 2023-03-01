708023b3a0b384182260d6464128f9689f3cde55
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-01 17:21:53 +0300

    добавил ещё констант

--- src/constatnts/links.ts
+++ src/constatnts/links.ts
@@ -5,5 +5,13 @@ export const MENU_ITEMS = [
 	{ display_name: "Услуги", link: "/services" },
 	{ display_name: "Студия", link: "/studio" },
 	{ display_name: "Портфолио", link: "/portfolio" },
+	// { display_name: "Блог", link: "/blog" },
+	// { display_name: "Войти", link: "/lk/sign " },
+];
+
+export const SOCIAL_LINKS = [
+	{ display_name: "Вконтакте", link: "https://vk.com/piybeep" },
+	{ display_name: "Ватсап", link: "https://web.whatsapp.com/" },
+	{ display_name: "Телеграм", link: "https://t.me/piybeep" },
 ];
 
