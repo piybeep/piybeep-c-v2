fbfeac1a1b73496267f75ee263483611d0240b84
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-01 17:08:25 +0300

    подготовка к разработке

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
 import Head from "next/head";
 import React from "react";
 
-import { Button } from "../src/components";
-import { FULL_SCREEN_FORM, SUCCESSFUL_SENDING } from "../src/constatnts";
-import { useModalWindow } from "../src/hooks";
-import { SuccessModalWindow, FullScreenForm, FormRequest } from "../src/modules";
-
 export default function Home() {
-	const { isHas, add, remove } = useModalWindow();
-
 	return (
 		<main>
 			<Head>
@@ -16,25 +9,6 @@ export default function Home() {
 				<meta name="description" content="" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<h1>
-				Welcome to <samp>piybeep</samp>
-			</h1>
-
-			<Button onClick={() => add(SUCCESSFUL_SENDING)} value="Open window" />
-			<Button onClick={() => add(FULL_SCREEN_FORM)} value="Open form" />
-
-			<FormRequest/>
-
-			<SuccessModalWindow
-				isShow={isHas(SUCCESSFUL_SENDING)}
-				close={() => remove(SUCCESSFUL_SENDING)}
-			/>
-
-			<FullScreenForm
-				isShow={isHas(FULL_SCREEN_FORM)}
-				close={() => remove(FULL_SCREEN_FORM)}
-			/>
 		</main>
 	);
 }
--- src/constatnts/links.ts
+++ src/constatnts/links.ts
@@ -1,1 +1,9 @@
 export const PRIVACY_LINK = "https://piybeep.com";
+
+export const MENU_ITEMS = [
+	{ display_name: "Главная", link: "/" },
+	{ display_name: "Услуги", link: "/services" },
+	{ display_name: "Студия", link: "/studio" },
+	{ display_name: "Портфолио", link: "/portfolio" },
+];
+
