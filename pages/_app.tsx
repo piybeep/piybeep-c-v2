f8832d3eadb3152426fb6ee7da280b8bd70f5f79
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-03 13:11:51 +0300

    upload fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,10 @@
 import "../src/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Inter, PT_Mono, Montserrat } from "@next/font/google";
 import classNames from "classnames";
-
-export const inter = Inter({
-	preload: true,
-	subsets: ["cyrillic", "latin"],
-	variable: "--font-Inter",
-	fallback: ["sans-serif"],
-});
-
-export const ptmono = PT_Mono({
-	preload: true,
-	weight: ["400"],
-	subsets: ["cyrillic", "latin"],
-	variable: "--font-PTMono",
-	fallback: ["sans-serif"],
-});
-
-export const montserrat = Montserrat({
-	preload: true,
-	weight: ["400"],
-	subsets: ["cyrillic", "latin"],
-	variable: "--font-Montserrat",
-	fallback: ["sans-serif"],
-});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div
-			className={classNames(
-				"wrapper",
-				inter.variable,
-				ptmono.variable,
-				montserrat.variable
-			)}
-		>
+		<div className={classNames("wrapper")}>
 			<Component {...pageProps} />
 		</div>
 	);
