45b649ce7fdcdb8f8338df5636b27a5379b96614
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-09 15:31:06 +0300

    рабочий слайдер через swiper

--- src/modules/OurProjectsBlock/OurProjectsBlock.tsx
+++ src/modules/OurProjectsBlock/OurProjectsBlock.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react";
+import React, { ComponentProps } from "react";
 import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
@@ -13,49 +13,99 @@ import s from "./OurProjectsBlock.module.scss";
 import LeftLong from "../../../public/svg/Arrows/LeftLong.svg";
 import RightLong from "../../../public/svg/Arrows/RightLong.svg";
 
-const ButtonGroup = (props: ComponentProps<"div">) => {
-	const { slides, realIndex, slidePrev, slideNext } = useSwiper();
-	const slidesToShow = slides.filter((slide) =>
-			slide.classList.value.includes("swiper-slide-active")
-		).length,
-		currentSlide = realIndex;
+const NextButton = () => {
+	const swiper = useSwiper();
 
-	console.log(slidesToShow, currentSlide, props);
+	return (
+		<Image
+			src={RightLong}
+			onClick={() => swiper.slideNext()}
+			alt="right-arrow"
+		/>
+	);
+};
 
-	const nums = Array(slidesToShow)
-		.fill(1)
-		.map((_, index) => slidesToShow - index + currentSlide)
+const PrevButton = () => {
+	const swiper = useSwiper();
+
+	return (
+		<Image src={LeftLong} onClick={() => swiper.slidePrev()} alt="left-arrow" />
+	);
+};
+
+const NavInfo = ({ groupCount }: { groupCount: number }) => {
+	const swiper = useSwiper();
+	const [info, setInfo] = React.useState({
+		activeSlide: 0,
+		totalSlides: 0,
+	});
+
+	React.useEffect(() => {
+		const updateActiveSlide = (data: any) => {
+			setInfo((v) => ({
+				...v,
+				activeSlide: data.activeIndex,
+			}));
+		};
+
+		if (swiper) {
+			swiper.on("activeIndexChange", updateActiveSlide);
+		}
+
+		return () => {
+			if (swiper) {
+				swiper.off("activeIndexChange", updateActiveSlide);
+			}
+		};
+	}, []);
+
+	React.useEffect(() => {
+		setInfo((v) => ({
+			...v,
+			totalSlides: swiper.slides.length,
+		}));
+	}, [swiper.slides]);
+
+	const infoText = Array(groupCount)
+		.fill(0)
+		.map((_, index) => groupCount - index + info.activeSlide)
 		.reverse()
 		.join(", ");
 
 	return (
-		<div {...props}>
-			<Image src={LeftLong} onClick={() => slidePrev()} alt="left-arrow" />
-			{nums} из {slides.length}
-			<Image src={RightLong} onClick={() => slideNext()} alt="right-arrow" />
-		</div>
+		<span>
+			{infoText} из {info.totalSlides}
+		</span>
 	);
 };
 
 export function OurProjectsBlock() {
+	const [groupCount, setGroupCount] = React.useState(3);
+	const [allowTouchMove, setAllowTouchMove] = React.useState(false);
 	return (
 		<BlockLayout title="Наши проекты ." subtitle="Скоро больше проектов">
 			<Swiper
 				className={s.slider}
 				direction="horizontal"
 				spaceBetween={40}
-				slidesPerView={3}
-				slidesPerGroup={3}
+				slidesPerView={groupCount}
+				slidesPerGroup={groupCount}
 				wrapperClass={s.wrapper}
 				preventClicks={false}
 				slideClass={s.items}
+				loop={false}
+				allowTouchMove={allowTouchMove}
 			>
 				{PRODUCTS.map((item) => (
 					<SwiperSlide key={item.id}>
 						<ProjectCard title={item.title} description={item.description} />
 					</SwiperSlide>
 				))}
-				<ButtonGroup className={s.buttonGroup} />
+				<div className={s.buttonGroup}>
+					<PrevButton />
+					<NavInfo groupCount={groupCount} />
+					<NextButton />
+				</div>
 			</Swiper>
 		</BlockLayout>
 	);
