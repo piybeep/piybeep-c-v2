bca41909507b365c8baedef235f63119cdb7f4c3
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-03-21 03:12:52 +0300

    add preview with menu

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import { HeaderProps } from './Header.types';
 import { CONTACTS_DATA, MENU_ITEMS } from '../../constatnts';
@@ -7,12 +9,59 @@ import { CONTACTS_DATA, MENU_ITEMS } from '../../constatnts';
 import s from './Header.module.scss'
 
 import logo from '../../../public/svg/logo.svg'
-import { useRouter } from 'next/router';
 
 export function Header({ ...props }: HeaderProps) {
-
     const query = useRouter()
-    console.log(query)
+    const [hidden, setHidden] = useState(false);
+
+    // Скрытие preview, пока тест ибо работает не стабильно
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setHidden(true)
+            }
+            if (window.scrollY == 0) {
+                setHidden(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => { window.removeEventListener('scroll', handleScroll) }
+    }, [hidden])
+
+    // Появление менюшки и её скрытие и открытие при помощи мыши, пока тест
+    useEffect(() => {
+        let elY = 0
+        let scrollY = 0
+        const scrollTop = () => {
+            const el = document.querySelector<HTMLElement>('.' + s.bar);
+            if (el != null) {
+                const height = el.offsetHeight;
+                const pos = window.pageYOffset;
+                const diff = scrollY - pos;
+
+                elY = Math.min(0, Math.max(-height, elY + diff));
+                el.style.position = pos >= height ? 'sticky' : pos === 0 ? 'sticky' : el.style.position;
+                el.style.transform = `translateY(${pos > 50 ? elY : 0}px)`;
+
+                scrollY = pos;
+            }
+        }
+
+        window.addEventListener('scroll', scrollTop)
+
+        const mouseCord = (mouse: { clientY: any; }) => {
+            const offsetY = mouse.clientY
+
+            const el = document.querySelector<HTMLElement>('.' + s.bar);
+
+            if (offsetY < 50 && el) {
+                el.style.transform = `translateY(0px)`
+            }
+        }
+
+        window.addEventListener('mousemove', mouseCord)
+    }, [])
+
 
     const links = MENU_ITEMS.map(current => {
         return (
@@ -23,8 +72,21 @@ export function Header({ ...props }: HeaderProps) {
     })
 
     return (
-        <div className={s.wrapper}>
-            <header className={s.header}>
+        <header className={classNames(s.header, {
+            [s.header__visible]: hidden,
+        })}>
+            <div className={classNames(s.preview,
+                {
+                    [s.preview__visible]: query.pathname === '/',
+                    [s.preview__hidden]: hidden,
+                })}>
+                <div className={s.preview__info}>
+                    <h2 className={s.preview__title}>web-studio</h2>
+                    <p className={s.preview__subtitle}>PIYBEEP</p>
+                </div>
+            </div>
+
+            <div className={s.bar}>
                 <Link href='/' className={s.logo}>
                     <img className={s.logo__svg} src={logo.src} alt="Логотип" />
                     <p className={s.logo__text}>piybeep.</p>
@@ -45,10 +107,8 @@ export function Header({ ...props }: HeaderProps) {
                         {CONTACTS_DATA.get('email')}
                     </Link>
                 </div>
-
-                <button className={s.burger}></button>
-            </header>
-        </div>
+            </div>
+        </header>
     );
 };
 
