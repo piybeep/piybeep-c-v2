bc1ae8162df688cff556b545c90f200aeb715eaf
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-03-22 09:08:56 +0300

    add about-us

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { ReactNode } from "react";
 import {
+    AboutUs,
 	AdvantagesBlock,
 	OurProjectsBlock,
 	Reviews,
@@ -20,6 +21,7 @@ export default function Home() {
 				marginTop: 50,
 			}}
 		>
+            <AboutUs/>
 			<OurProjectsBlock />
 			<AdvantagesBlock />
 			<RojectsPreview />
---
+++ src/modules/AboutUs/AboutUs.tsx
@@ -0,0 +1,19 @@
+import { AboutUsProps } from "./AboutUs.types";
+
+import aboutImg from '../../../public/imgs/about-us.png'
+
+import s from './AboutUs.module.scss'
+
+export function AboutUs({...props}: AboutUsProps){
+    return (
+        <div className={s.wrapper}>
+            <h2 className={s.title}>Продающие сайты для ваших маркетинговых целей.</h2>
+            <div className={s.info}>
+                <h3 className={s.info__slogan}>piybeep. разрабатывает продающие сайты для компаний, которые хотят уверенно овладеть таким каналом продаж, либо улучшить его и сделать свой интернет-маркетинг эффективнее</h3>
+                <img className={s.info__img} src={aboutImg.src} alt="Картинка" />
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
