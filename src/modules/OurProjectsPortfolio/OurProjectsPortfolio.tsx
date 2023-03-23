ae6fbdd610180ec207c3c116bab5ff500c382ecb
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-24 00:08:39 +0300

    Update OurProjectsPortfolio.tsx
    Signed-off-by: Даня <45875910+danyatochkaru@users.noreply.github.com>

--- src/modules/OurProjectsPortfolio/OurProjectsPortfolio.tsx
+++ src/modules/OurProjectsPortfolio/OurProjectsPortfolio.tsx
@@ -6,7 +6,7 @@ import s from './OurProjectsPortfolio.module.scss'
 
 export function OurProjectsPortfolio (){
   return (
-    <BlockLayout value='Наши проекты' position='center' subtitle='123'>
+    <BlockLayout value='Наши проекты' position='center'>
       <div className={s.wrapper}>
         {PRODUCTS.map((el) => (
           <ProjectCard
