79861e7591e11ce03122f3c49c892797a109c3ad
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-23 13:38:02 +0300

    Squashed commit of the following:
    commit 4c56fd3155bb047622558cb8bee4e0a2a06684f8
    Author: Даня <45875910+danyatochkaru@users.noreply.github.com>
    Date:   Thu Mar 23 13:35:52 2023 +0300
    
        fix images
    
    commit bbd0fbd4293ed5776f617caa84d16aaf0ae7e863
    Author: ivan <ivan.safonov.09@mail.ru>
    Date:   Wed Mar 22 20:53:43 2023 +0300
    
        add OurProjectsPortfolio

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,25 +1,22 @@
-import Head from "next/head";
-import { ReactNode } from "react";
-import { BaseLayout } from "../../src/layouts";
-
+import Head from 'next/head'
+import { ReactNode } from 'react'
+import { BaseLayout} from '../../src/layouts'
+import { OurProjectsPortfolio } from '../../src/modules'
 export default function Portfolio() {
-	return (
-		<main>
-			<h1>
-				Портфолио <samp>Piybeep</samp>
-			</h1>
-		</main>
-	);
+  return (
+    <main>
+		<OurProjectsPortfolio/>
+    </main>
+  )
 }
 
 Portfolio.getLayout = (page: ReactNode) => (
-	<BaseLayout>
-		<Head>
-			<title>Piybeep - Портфолио</title>
-			<meta name="description" content="Наше портфолио" />
-			<link rel="icon" href="/favicon.ico" />
-		</Head>
-		{page}
-	</BaseLayout>
-);
-
+  <BaseLayout>
+    <Head>
+      <title>Piybeep - Портфолио</title>
+      <meta name='description' content='Наше портфолио' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    {page}
+  </BaseLayout>
+)
---
+++ src/modules/OurProjectsPortfolio/index.tsx
@@ -0,0 +1,1 @@
+export {OurProjectsPortfolio} from './OurProjectsPortfolio'
