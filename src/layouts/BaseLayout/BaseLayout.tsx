e6744cc832cdec2094464100d36d267d4069237c
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-03-24 18:46:10 +0300

    Update BaseLayout.tsx
    Signed-off-by: Даня <45875910+danyatochkaru@users.noreply.github.com>

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,13 +1,6 @@
 import { Footer, Form, Header } from "../../modules";
+
 export function BaseLayout({ children }: { children: React.ReactNode }) {
-	// const options = {
-	// 	damping: 0.07
-	// }
-
-	// useEffect(() => {
-	// 	Scrollbar.init(document.body, options);
-	// }, [])
-
 	return (
 		<>
 			<Header />
