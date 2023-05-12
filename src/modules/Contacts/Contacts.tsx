8ea26ce46b6b7e73f07ada7cb5eb4ff7c426fafd
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-05-12 15:06:11 +0300

    Студия. Блок "Всегда на связи"  (#53)
    * add contacts
    
    * add key in social

---
+++ src/modules/Contacts/Contacts.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Title } from '../../components';
+
+import { CONTACTS_DATA, SOCIAL_LINKS } from '../../constatnts';
+
+import { ContactsProps } from './Contacts.types';
+import s from './Contacts.module.scss'
+
+export function Contacts({ }: ContactsProps) {
+    return (
+        <div className={s.wrapper}>
+            <Title value={'Всегда на связи'} />
+            <div className={s.list}>
+            <div className={s.contact}>
+                <Link className={s.contact__link} href={`tel:${CONTACTS_DATA.get("phone")}`}>
+                    {CONTACTS_DATA.get("phone")}
+                </Link>
+                <Link className={s.contact__link} href={`tel:${CONTACTS_DATA.get("phone")}`}>
+                    {CONTACTS_DATA.get("phone")}
+                </Link>
+                <Link className={s.contact__link} href={`mailto:${CONTACTS_DATA.get("email")}`}>
+                    {CONTACTS_DATA.get("email")}
+                </Link>
+            </div>
+            <span className={s.slogan}>
+                Мы всегда на связи!  Оставляйте заявку на сайте, звоните по телефону, пишите нам на почту, в телеграм, ватсап или вконтакте, и мы обсудим ваш проект!
+            </span>
+            <div className={s.social}>
+                {SOCIAL_LINKS.map(current => (
+                    <Link key={current.display_name} href={current.link} className={s.social__link}>
+                        <Image className={s.social__img} src={current.icon} alt={'Картинка'} />
+                        {current.display_name}
+                    </Link>
+                ))}
+            </div>
+                </div>
+        </div>
+    );
+};
