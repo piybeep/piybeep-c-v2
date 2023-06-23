1f169ec7ba496de35c994226420ba5acbea20d2f
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-06-23 12:34:34 +0300

    feat: сделал "мы делаем" для бизнеса

--- src/modules/WeDo/WeDo.tsx
+++ src/modules/WeDo/WeDo.tsx
@@ -1,22 +1,31 @@
-import {BlockLayout} from '../../layouts';
-import {WeDoProps} from './WeDo.types';
-import {WE_DO_LIST} from '../../constatnts/weDo';
+import { BlockLayout } from "../../layouts";
+import { WeDoProps } from "./WeDo.types";
+import { WE_DO_LIST, WE_DO_LIST_BIZ } from "../../constatnts/weDo";
 
-import s from './WeDo.module.scss'
+import s from "./WeDo.module.scss";
+import classNames from "classnames";
 
-export function WeDo({...props}: WeDoProps) {
-    return (
-        <BlockLayout value="Мы делаем">
-            <div className={s.list}>
-                {WE_DO_LIST.map((current, index) => {
-                    return (
-                        <div key={current.title} className={s.info} style={{marginLeft: (index * 8) + '%'}}>
-                            <h2 className={s.info__title}>{current.title}</h2>
-                            <h3 className={s.info__subtitle}>{current.text}</h3>
-                        </div>
-                    )
-                })}
-            </div>
-        </BlockLayout>
-    );
+export function WeDo({ biz = false, ...props }: WeDoProps) {
+	return (
+		<BlockLayout value="Мы делаем">
+			<div
+				className={classNames(s.list, {
+					[s.biz]: biz,
+				})}
+			>
+				{(biz ? WE_DO_LIST_BIZ : WE_DO_LIST).map((current, index) => {
+					return (
+						<div
+							key={current.title}
+							className={s.info}
+							style={{ marginLeft: index * 8 + "%" }}
+						>
+							<h2 className={s.info__title}>{current.title}</h2>
+							<h3 className={s.info__subtitle}>{current.text}</h3>
+						</div>
+					);
+				})}
+			</div>
+		</BlockLayout>
+	);
 }
