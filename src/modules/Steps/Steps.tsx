42a6bda9f737be960eab5a27d4f979da30eedab5
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-07-04 22:46:01 +0300

    fix: Удалил ненужны stepsProps и переделал структуру в steps

--- src/modules/Steps/Steps.tsx
+++ src/modules/Steps/Steps.tsx
@@ -1,43 +1,34 @@
 import classNames from 'classnames';
 import { STEPS_INFO } from '../../constatnts/steps';
 import s from './Steps.module.scss'
-import { StepsProps } from './Steps.types';
 
-export function Steps({ ...props }: StepsProps) {
-
-    const Info = ({ ...props }) => {
-        return (
-            <>
-                {
-                    STEPS_INFO.map((current, index) => (
-                        <div className={s.info} key={current.header}>
-                            <div className={s.header}>
-                                <span className={s.header__number}>{`0${index + 1}`}</span>
-                                <h1 className={s.header__title}>{current.header}</h1>
-                                <h2 className={s.header__slogan}>{current.slogan}</h2>
-                            </div>
-                            <div className={s.list}>
-                                {current.list.map((current, index) => (
-                                    <div className={classNames(s.item, {
-                                        [s.item__right]: index === 1
-                                    })} key={current.title}>
-                                        <div className={s.item__info}>
-                                            <h3 className={s.item__title}>{current.title}</h3>
-                                            <h4 className={s.item__text}>{current.text}</h4>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))
-                }
-            </>
-        )
-    }
+export function Steps() {
 
     return (
         <div className={s.wrapper}>
-            <Info />
+            {
+                STEPS_INFO.map((current, index) => (
+                    <div className={s.info} key={current.header}>
+                        <div className={s.header}>
+                            <span className={s.header__number}>{`0${index + 1}`}</span>
+                            <h1 className={s.header__title}>{current.header}</h1>
+                            <h2 className={s.header__slogan}>{current.slogan}</h2>
+                        </div>
+                        <div className={s.list}>
+                            {current.list.map((current, index) => (
+                                <div className={classNames(s.item, {
+                                    [s.item__right]: index === 1
+                                })} key={current.title}>
+                                    <div className={s.item__info}>
+                                        <h3 className={s.item__title}>{current.title}</h3>
+                                        <h4 className={s.item__text}>{current.text}</h4>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 };
