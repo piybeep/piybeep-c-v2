cbfc9a98f953f9ac74dc12a33dd01272f0913a6d
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-07-05 15:26:58 +0300

    perf: почистил пустые файлы types

--- src/modules/PopupAuth/PopupAuth.tsx
+++ src/modules/PopupAuth/PopupAuth.tsx
@@ -1,108 +1,120 @@
-import {useRouterQuery} from "../../hooks";
+import { useRouterQuery } from "../../hooks";
 
 import * as Yup from "yup";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import classNames from "classnames";
 
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 
-import {Input, Title} from "../../components";
+import { Input, Title } from "../../components";
 
-import {PopupAuthProps} from "./PopupAuth.types";
-import s from './PopupAuth.module.scss'
+import s from "./PopupAuth.module.scss";
 
-export function PopupAuth({}: PopupAuthProps) {
-    const [isOpen, setIsOpen] = useState(false)
+export function PopupAuth() {
+	const [isOpen, setIsOpen] = useState(false);
 
-    const {isHas, query, mutate} = useRouterQuery()
+	const { isHas, query, mutate } = useRouterQuery();
 
-    useEffect(() => {
-        if (isHas('form') && query.form === 'auth') {
-            setIsOpen(true)
-        } else {
-            setIsOpen(false)
-        }
-    }, [isHas('form'), query.form])
+	useEffect(() => {
+		if (isHas("form") && query.form === "auth") {
+			setIsOpen(true);
+		} else {
+			setIsOpen(false);
+		}
+	}, [isHas("form"), query.form]);
 
-    useEffect(() => {
-        if (isOpen) {
-            document.body.style.overflow = 'hidden'
-        } else {
-            document.body.style.overflow = 'unset'
-        }
-    }, [isOpen])
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "unset";
+		}
+	}, [isOpen]);
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        }),
-        onSubmit: values => {
-            console.log(values)
-        }
-    })
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		validationSchema: Yup.object({
+			email: Yup.string().email().required(),
+			password: Yup.string().required(),
+		}),
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
 
-    const handleCloseForm = () => {
-        mutate({
-            query: {form: null},
-        })
-    }
+	const handleCloseForm = () => {
+		mutate({
+			query: { form: null },
+		});
+	};
 
-    return (
-        <div onClick={() => handleCloseForm()} className={classNames(s.wrapper, {
-            [s.wrapper__open]: isOpen
-        })}>
-            <form onSubmit={formik.handleSubmit} onClick={e => e.stopPropagation()} className={s.form}>
-                <div className={s.title}>
-                    <Title value="авторизация"/>
-                </div>
-                <div className={s.inputs}>
-                    <Input
-                        name="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        text="Почта"
-                        required
-                    />
-                    <Input
-                        name="password"
-                        required
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        text="Пароль"
-                    />
-                </div>
-                <div className={s.privacy}>
-                    Отправляя форму, вы принимаете <span
-                    className={s.privacy__link}>политику конфиденциальности</span> и <span className={s.privacy__link}>пользовательское соглашение</span>
-                </div>
+	return (
+		<div
+			onClick={() => handleCloseForm()}
+			className={classNames(s.wrapper, {
+				[s.wrapper__open]: isOpen,
+			})}
+		>
+			<form
+				onSubmit={formik.handleSubmit}
+				onClick={(e) => e.stopPropagation()}
+				className={s.form}
+			>
+				<div className={s.title}>
+					<Title value="авторизация" />
+				</div>
+				<div className={s.inputs}>
+					<Input
+						name="email"
+						onChange={formik.handleChange}
+						value={formik.values.email}
+						text="Почта"
+						required
+					/>
+					<Input
+						name="password"
+						required
+						onChange={formik.handleChange}
+						value={formik.values.password}
+						text="Пароль"
+					/>
+				</div>
+				<div className={s.privacy}>
+					Отправляя форму, вы принимаете{" "}
+					<span className={s.privacy__link}>политику конфиденциальности</span> и{" "}
+					<span className={s.privacy__link}>пользовательское соглашение</span>
+				</div>
 
-                <div className={s.buttons}>
-                    <button
-                        className={s.buttons__sumbit}
-                        type='submit'
-                    >
-                        Войти
-                        <svg className={s.buttons__svg} width="12" height="11" viewBox="0 0 12 11" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path className={s.buttons__svg}
-                                  d="M6.60227 11L5.72727 10.1364L9.32955 6.53409H0V5.28409H9.32955L5.72727 1.69318L6.60227 0.818182L11.6932 5.90909L6.60227 11Z"
-                                  fill="#8E8E8E"/>
-                        </svg>
-                    </button>
-                    <button
-                        className={s.buttons__close}
-                        type='button'
-                        onClick={() => handleCloseForm()}
-                    >
-                        Отмена
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
+				<div className={s.buttons}>
+					<button className={s.buttons__sumbit} type="submit">
+						Войти
+						<svg
+							className={s.buttons__svg}
+							width="12"
+							height="11"
+							viewBox="0 0 12 11"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								className={s.buttons__svg}
+								d="M6.60227 11L5.72727 10.1364L9.32955 6.53409H0V5.28409H9.32955L5.72727 1.69318L6.60227 0.818182L11.6932 5.90909L6.60227 11Z"
+								fill="#8E8E8E"
+							/>
+						</svg>
+					</button>
+					<button
+						className={s.buttons__close}
+						type="button"
+						onClick={() => handleCloseForm()}
+					>
+						Отмена
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
--- src/modules/Team/Team.tsx
+++ src/modules/Team/Team.tsx
@@ -1,49 +1,52 @@
-import {useEffect, useState} from 'react';
-import Image from 'next/image';
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-import {Title} from '../../components';
+import { Title } from "../../components";
 
-import {PERSON_LIST} from '../../constatnts/person';
+import { PERSON_LIST } from "../../constatnts/person";
 
-import {TeamProps} from './Team.types';
-import s from './Team.module.scss';
+import s from "./Team.module.scss";
 
-export function Team({}: TeamProps) {
+export function Team() {
+	const [windowSize, setWindowSize] = useState(11);
 
-    const [windowSize, setWindowSize] = useState(11)
+	useEffect(() => {
+		if (window) {
+			if (window.screen.width > 1550) {
+				setWindowSize(11);
+			} else if (window.screen.width < 1551 && window.screen.width > 1270) {
+				setWindowSize(10.5);
+			}
+		}
+	}, []);
 
-    useEffect(() => {
-        if (window) {
-            if (window.screen.width > 1550) {
-                setWindowSize(11)
-            } else if (window.screen.width < 1551 && window.screen.width > 1270) {
-                setWindowSize(10.5)
-            }
-        }
-    }, [])
+	const PersonInfo = ({ current }: { current: any }) => {
+		return (
+			<div
+				className={s.person}
+				style={{
+					zIndex: PERSON_LIST.length - current.id,
+					left: windowSize * current.id + "%",
+				}}
+			>
+				<Image className={s.person__img} src={current.img} alt={"Картинка"} />
+				<div className={s.person__info}>
+					<h2 className={s.person__title}>{current.title}</h2>
+					<h3 className={s.person__text}>{current.text}</h3>
+				</div>
+			</div>
+		);
+	};
 
-    const PersonInfo = ({current}: { current: any }) => {
-        return (
-            <div className={s.person}
-                 style={{zIndex: PERSON_LIST.length - current.id, left: windowSize * current.id + '%'}}>
-                <Image className={s.person__img} src={current.img} alt={'Картинка'}/>
-                <div className={s.person__info}>
-                    <h2 className={s.person__title}>{current.title}</h2>
-                    <h3 className={s.person__text}>{current.text}</h3>
-                </div>
-            </div>
-        )
-    }
+	return (
+		<div className={s.wrapper}>
+			<Title value={"Команда"} />
 
-    return (
-        <div className={s.wrapper}>
-            <Title value={'Команда'}/>
-
-            <div className={s.wrapper__list}>
-                {PERSON_LIST.map(current => (
-                    <PersonInfo key={current.id} current={current}/>
-                ))}
-            </div>
-        </div>
-    );
+			<div className={s.wrapper__list}>
+				{PERSON_LIST.map((current) => (
+					<PersonInfo key={current.id} current={current} />
+				))}
+			</div>
+		</div>
+	);
 }
