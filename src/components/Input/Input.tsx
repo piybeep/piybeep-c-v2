48eb21504ea51127b7dababd2749962a86bd882b
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-07-10 23:06:48 +0300

    fix: исправил работу laceholder теперь всё работает

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,7 +8,7 @@ import s from './Input.module.scss'
 export function Input({text, position = 'top', sizeInput = 'default', ...props}: InputProps) {
     return (
         <div className={classNames(s.input, s['input_' + position])}>
-            <input {...props} placeholder={text} className={classNames(s.input__text, s['input__text_' + sizeInput])}/>
+            <input {...props} placeholder={text} className={classNames(s.input__text, s['input__text_' + sizeInput], s['input__text_' + position])}/>
             <span
                 className={classNames(s.input__placeholder, s['input__placeholder_' + position], s['input__placeholder_' + sizeInput])}>{text}</span>
         </div>
