317e8754ecf4da8c9ca16904289a4819ad7445d0
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-01 22:24:09 +0300

    feat: настроил sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+	siteUrl: "https://piybeep.com",
+	exclude: ["/404"],
+	robotsTxtOptions: {
+		policies: [
+			{ userAgent: "*", disallow: ["/404", "/api/*"] },
+			{ userAgent: "*", allow: "/" },
+		],
+	},
+	generateRobotsTxt: true,
+};
