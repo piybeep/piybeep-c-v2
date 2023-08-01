57f6b5f21a932ab546eb74cbf0824d3fa80b9879
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-08-01 21:45:55 +0300

    fix: исправил ошибку конфига

--- next.config.js
+++ next.config.js
@@ -10,15 +10,20 @@ const nextConfig = {
 	productionBrowserSourceMaps: true,
 	async headers() {
 		return [
-			{ key: "X-Content-Type-Options", value: "nosniff" },
 			{
-				key: "Strict-Transport-Security",
-				value: "max-age=31536000; includeSubDomains",
+				source: "/:path*",
+				headers: [
+					{ key: "X-Content-Type-Options", value: "nosniff" },
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=31536000; includeSubDomains",
+					},
+					{ key: "X-Frame-Options", value: "DENY" },
+					{ key: "X-XSS-Protection", value: "1; mode=block" },
+					{ key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+					{ key: "Referrer-Policy", value: "no-referrer" },
+				],
 			},
-			{ key: "X-Frame-Options", value: "DENY" },
-			{ key: "X-XSS-Protection", value: "1; mode=block" },
-			{ key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-			{ key: "Referrer-Policy", value: "no-referrer" },
 		];
 	},
 };
