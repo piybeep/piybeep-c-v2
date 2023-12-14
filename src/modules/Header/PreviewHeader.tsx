f047d3c707dcee0ef9fc07c0b4844b5f3bc06969
Daniil <support@danyatochka.ru>
2023-12-15 00:35:15 +0300

    feat: эффект снегопада в preview header

--- src/modules/Header/PreviewHeader.tsx
+++ src/modules/Header/PreviewHeader.tsx
@@ -4,10 +4,11 @@ import classNames from "classnames";
 import { useRouter } from "next/router";
 
 import s from "./Header.module.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PAGES_LINK } from "../../constatnts";
 import { useInView } from "react-intersection-observer";
 import { useApp } from "../../store";
+import Snowfall from "react-snowfall";
 
 export function PreviewHeader() {
 	const query = useRouter();
@@ -20,6 +21,11 @@ export function PreviewHeader() {
 		},
 	});
 
+	const [isClient, setIsClient] = useState(false);
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
 	return (
 		<header
 			className={classNames(s.preview, {
@@ -28,6 +34,16 @@ export function PreviewHeader() {
 					query.pathname === PAGES_LINK.BUSINESS,
 			})}
 		>
+			{isClient && (
+				<Snowfall
+					snowflakeCount={100}
+					speed={[0.5, 2]}
+					wind={[0, 4]}
+					radius={[0.5, 3]}
+					color={"#fffa"}
+					rotationSpeed={[0, 2]}
+				/>
+			)}
 			<div className={s.preview__info} ref={ref}>
 				<h2 className={s.preview__title}>web-studio</h2>
 				<p className={s.preview__subtitle}>
