17787fe175ca2bfef485bdf68eb30c71104f41d0
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-01-19 13:45:23 +0300

    feat: Вывел spa в contants, но есть нюанс, я с типами немного не понял как пофиксить, пришлось через тернарник у (pos?: 'left' | 'right)

--- src/modules/pages/studio/Spa/Spa.tsx
+++ src/modules/pages/studio/Spa/Spa.tsx
@@ -3,28 +3,22 @@ import coder from '../../../../../public/animtate/coder.json'
 
 import s from "./Spa.module.scss";
 import { Header, Info } from "./components";
+import { SPA_INFO } from "../../../../constatnts";
 
 export function Spa() {
 	return (
 		<div className={s.wrapper}>
 			<Header title="Мы разрабатываем" text="Spa" />
 			<div className={s.info}>
-				<Info
-					title="Что такое SPA?"
-					text="SPA - single page applications (одностраничные приложения) - это
-						самая современная технология разработки сайтов, которая позволяет
-						сделать ресурс максимально быстрым, удобным и безопасным,
-						оптимизировать для лучшей выдачи в поисковых системах
-						(SEO-оптимизация) и выполнить поставленную заказчиком задачу."
-				/>
-				<Info
-					pos='right'
-					title="А подробнее..."
-					text="Хоть и называется “приложение” - на самом деле это обыкновенный
-						сайт, каким вы привыкли его видеть. “Одностраничное” - потому что
-						используется одна оболочка для загрузки информации, а весь
-						последующий контент загружается динамически без перезагрузки,
-						поэтому количество страниц может быть каким угодно." />
+				{
+					SPA_INFO?.map(item =>
+						<Info
+							key={item.title}
+							title={item.title}
+							text={item.text}
+							pos={item?.pos === 'left' ? 'left' : 'right'} />
+					)
+				}
 			</div>
 			<div className={s.block}>
 				<LottieAnimation loop={true} animation={coder} />
