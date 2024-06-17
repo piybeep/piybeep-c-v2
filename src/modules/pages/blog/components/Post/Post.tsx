e796ce64de84fae8ee55900011fcd43484b761e4
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-06-17 19:07:10 +0300

    fix: поправил font-size текста в услугах

--- src/modules/pages/blog/components/Post/Post.tsx
+++ src/modules/pages/blog/components/Post/Post.tsx
@@ -8,7 +8,6 @@ import { BlogsTypes } from '../../../../../types';
 export function Post({ post }: { post: BlogsTypes }) {
     return (
         <Link className={s.post} href={`/blog/${post.id}`}>
-            {post.id}
             <Image className={s.post__img} src={process.env.NEXT_PUBLIC_STRAPI_URL + post?.previewImage} alt={''} width={520} height={346} />
             <div className={s.post__info}>
                 <Slogan text={post.title} />
