27e1d50923b1e49ce16ccc88c4f4aae44b7f5095
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-06-24 22:36:53 +0300

    feat: добавил сортировку по rank (dragDrop)

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -38,11 +38,9 @@ export const getServerSideProps: GetServerSideProps = async (_ctx) => {
 
 	const [projects_response, services_response] = await Promise.allSettled(
 		URIs.map((i) => axios.get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/${i}?${qs.stringify(Object.assign({ populate: '*' },
-			i === 'services'
-				? undefined
-				: {
-					sort: 'createdAt:desc'
-				}
+			{
+				sort: 'rank:asc'
+			}
 		))
 			}`, {
 			headers: {
