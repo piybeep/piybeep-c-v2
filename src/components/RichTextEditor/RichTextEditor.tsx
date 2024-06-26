7d1ff3f7afa29034da12cedc57f83d36940f09c0
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-06-26 19:32:07 +0300

    fix: убрал ненужный вывод в консоль

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,7 +1,6 @@
 import s from './RichTextEditor.module.scss'
 
 export function RichTextEditor({ html }: { html: string }) {
-    console.log(html)
     return (
         <div className={s.richTextEditor} dangerouslySetInnerHTML={{ __html: html }} />
     );
