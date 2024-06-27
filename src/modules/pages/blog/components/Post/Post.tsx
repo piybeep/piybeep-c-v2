3af6270a1506d025830139bdf9cb45f3c18d35ec
Daniil <support@danyatochka.ru>
2024-06-27 22:27:09 +0300

    fix: удалил id на карточках постов

--- src/modules/pages/blog/components/Post/Post.tsx
+++ src/modules/pages/blog/components/Post/Post.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Marker } from '../Marker';
-import { Slogan } from '../Slogan';
-import s from './Post.module.scss';
-import { BlogsResTypes } from '../../../../../types';
+import Image from "next/image";
+import Link from "next/link";
+import { Marker } from "../Marker";
+import { Slogan } from "../Slogan";
+import s from "./Post.module.scss";
+import { BlogsResTypes } from "../../../../../types";
 
 export function Post({ post }: { post: BlogsResTypes }) {
     return (
         <Link className={s.post} href={`/blog/${post.slug}`}>
-            {post.id}
             <Image className={s.post__img} src={process.env.NEXT_PUBLIC_STRAPI_URL! + post?.image_preview.url} alt={''} width={520} height={346} />
             <div className={s.post__info}>
                 <Slogan text={post.title} />
