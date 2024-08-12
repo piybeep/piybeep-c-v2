4be042de78f3f0c1dc490efe1d7562ce257eece7
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-08-12 20:39:03 +0300

    feat: добавил картинки в steps

--- src/modules/Steps/Steps.tsx
+++ src/modules/Steps/Steps.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { STEPS_INFO } from "../../constatnts/steps";
 import s from "./Steps.module.scss";
 import { Header, Item } from "./components";
@@ -9,9 +10,22 @@ export function Steps() {
 				<div className={s.info} key={current.header}>
 					<Header number={`${index + 1}`} title={current.header} slogan={current.slogan} />
 					<div className={s.list}>
-						{current.list.map((current, index) => (
-							<Item key={current.title} index={index} title={current.title} text={current.text} />
-						))}
+						{
+							current.list.map((item, index) => {
+								if (index === 0) {
+									return (
+										<>
+											<Item key={item.title} index={index} title={item.title} text={item.text} />
+											<Image className={s.list__img} src={current.image} alt="" width={450} height={450} />
+										</>
+									)
+								} else {
+									return (
+										<Item key={item.title} index={index} title={item.title} text={item.text} />
+									)
+								}
+							})
+						}
 					</div>
 				</div>
 			))}
