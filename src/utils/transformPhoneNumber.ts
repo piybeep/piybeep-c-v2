689e717a76d7e3273a447094919d728abd05e6a2
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-08-14 18:09:33 +0300

    feat: добавил проверку для трансформации

--- src/utils/transformPhoneNumber.ts
+++ src/utils/transformPhoneNumber.ts
@@ -3,5 +3,5 @@ export default function transformPhoneNumber(phone: string) {
         ? [[phone.substring(0, 2), phone.substring(2, 5), phone.substring(5, 8)].join(" "), phone.substring(8, 10), phone.substring(10)].join("-")
         : [[phone.substring(0, 1), phone.substring(1, 4), phone.substring(4, 7)].join(" "), phone.substring(7, 9), phone.substring(9)].join("-")
 
-    return transformPhone
+        return phone.split('').length <= 12 ? transformPhone : phone
 }
