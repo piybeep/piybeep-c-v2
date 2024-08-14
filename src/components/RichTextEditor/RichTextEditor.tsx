6fd7ed40631fb665ccd72638640b32048ac5943b
Danya <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-08-14 18:21:10 +0300

    fix: поправил отступ у первого p (#115)

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import s from './RichTextEditor.module.scss'
 
 export function RichTextEditor({ html }: { html: string }) {
+    useEffect(() => {
+        const block = document.getElementById('richTextEditor')
+
+        block?.children[0].tagName.toLowerCase() === 'p' && block.children[0].classList.add(s.richTextEditor__first)
+    }, [])
     return (
-        <div className={s.richTextEditor} dangerouslySetInnerHTML={{ __html: html }} />
+        <div className={s.richTextEditor} id='richTextEditor' dangerouslySetInnerHTML={{ __html: html }} />
     );
 };
