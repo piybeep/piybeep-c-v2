9d18924579628d436f572c3e4c66567739e8e90e
Daniil <support@danyatochka.ru>
2024-08-17 15:33:13 +0300

    fix: поправил robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,8 +5,11 @@ module.exports = {
 	exclude: ["/404"],
 	robotsTxtOptions: {
 		policies: [
-			{ userAgent: "*", disallow: ["/404", "/api/*", "/*?form*", ...Array(12).fill(0).map((_, i) => `/portfolio/${i+1}`)] },
-			{ userAgent: "*", allow: ["/"] }
+			{
+				userAgent: "*",
+				disallow: ["/404", "/api/*", "/*?form*", ...Array(12).fill(0).map((_, i) => `/portfolio/${i+1}`)],
+				allow: ["/"]
+			},
 		]
 	},
 	generateRobotsTxt: true
